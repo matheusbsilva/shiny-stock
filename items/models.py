5a202d67e185b4690d9fdeb0056dc3c7fa06d3da
matheusbsilva <matheusbattista@hotmail.com>
2017-12-31 17:32:20 -0200

    [UPDATE] Create models for the item app     - Item     - ItemType     - UnityType     - History

--- items/models.py
+++ items/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class ItemType(models.Model):
+    description_item = models.CharField(max_length=50,blank=False,null=False)
+
+
+class UnityType(models.Model):
+    description_unity = models.CharField(max_length=50,blank=False,null=False)
+
+
+class Item(models.Model):
+    name_item = models.CharField(max_length=50,blank=False,null=False)
+    quantity_item = models.IntegerField(blank=False,null=False,default=0)
+    item_type = models.ForeignKey(ItemType)
+    unity_type = models.ForeignKey(UnityType)
+
+
+OPERATION_TYPES = (
+    """ Operation types are in portuguese-BR """
+    ('0','Item inserido'),
+    ('1','Item retirado'),
+)
+
+
+class History(models.Model):
+    date = DateField(blank=False,null=False)
+    operation_type = models.CharField(
+            max_length=1,
+            choices=OPERATION_TYPES,
+            blank=False,
+            null=False)
+    item = models.ForeignKey(Item)
