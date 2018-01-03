32ea90fdb041d526c45b4672ad75ea5c5dff3a0b
matheusbsilva <matheusbattista@hotmail.com>
2018-01-03 18:36:56 -0200

    [UPDATE] Add on_delete param on model class related

--- items/models.py
+++ items/models.py
@@ -11,22 +11,22 @@ class UnityType(models.Model):
 class Item(models.Model):
     name_item = models.CharField(max_length=50,blank=False,null=False)
     quantity_item = models.IntegerField(blank=False,null=False,default=0)
-    item_type = models.ForeignKey(ItemType)
-    unity_type = models.ForeignKey(UnityType)
+    item_type = models.ForeignKey(ItemType,on_delete=models.CASCADE)
+    unity_type = models.ForeignKey(UnityType,on_delete=models.CASCADE)
 
 
+""" Operation types are in portuguese-BR """
 OPERATION_TYPES = (
-    """ Operation types are in portuguese-BR """
-    ('0','Item inserido'),
-    ('1','Item retirado'),
+    ('0','Item retirado'),
+    ('1','Item inserido'),
 )
 
 
 class History(models.Model):
-    date = DateField(blank=False,null=False)
+    date = models.DateField(auto_now=True,blank=False,null=False)
     operation_type = models.CharField(
             max_length=1,
             choices=OPERATION_TYPES,
             blank=False,
             null=False)
-    item = models.ForeignKey(Item)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
