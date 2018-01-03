0b0a90a42b98aeb5e66e3e950eaaa443bc95df20
matheusbsilva <matheusbattista@hotmail.com>
2018-01-03 18:47:10 -0200

    [UPDATE] Add __str__ method to models of items

--- items/models.py
+++ items/models.py
@@ -3,17 +3,25 @@ from django.db import models
 class ItemType(models.Model):
     description_item = models.CharField(max_length=50,blank=False,null=False)
 
+    def __str__(self):
+        return self.description_item
+
 
 class UnityType(models.Model):
     description_unity = models.CharField(max_length=50,blank=False,null=False)
     size_unity = models.CharField(max_length=10,blank=True,null=True)
 
+    def __str__(self):
+        return self.description_unity
 
 class Item(models.Model):
     name_item = models.CharField(max_length=50,blank=False,null=False)
     quantity_item = models.IntegerField(blank=False,null=False,default=0)
     item_type = models.ForeignKey(ItemType,on_delete=models.CASCADE)
     unity_type = models.ForeignKey(UnityType,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_item
 
 
 """ Operation types are in portuguese-BR """
@@ -31,3 +39,6 @@ class History(models.Model):
             blank=False,
             null=False)
     item = models.ForeignKey(Item,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (str(self.date) + '-' + self.item.name_item)
