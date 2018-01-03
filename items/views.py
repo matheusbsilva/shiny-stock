5164a4d971fc6b637cf85e1cebd368ff44c86a5f
matheusbsilva <matheusbattista@hotmail.com>
2018-01-03 20:06:01 -0200

    [UPDATE] Refact views to use generic and create item detail view

--- items/views.py
+++ items/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.views import generic
 
 from .models import Item
 
-def index(request):
-    item_list = Item.objects.all() 
-    template = loader.get_template('items/index.html')
-    context = {
-        'item_list': item_list,
-    }
-    
-    return HttpResponse(template.render(context,request))
+class IndexView(generic.ListView):
+    template_name = 'items/index.html'
+    context_object_name = 'item_list'
+
+    def get_queryset(self):
+        return Item.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Item
+    template_name = 'items/detail.html'
