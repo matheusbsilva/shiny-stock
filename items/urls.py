65e27d6e200a780da8c858ff63589df8b88fb504
matheusbsilva <matheusbattista@hotmail.com>
2018-01-04 17:30:06 -0200

    [UPDATE] Create view method and template to create a new item

--- items/urls.py
+++ items/urls.py
@@ -6,4 +6,5 @@ app_name = 'items'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('new',views.new_item,name='new'),
 ]
