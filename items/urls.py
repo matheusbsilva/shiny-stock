323e34826deac2539ff4794ae769254922fcbd07
matheusbsilva <matheusbattista@hotmail.com>
2018-01-04 17:37:41 -0200

    [UPDATE] Refact create item method of items views

--- items/urls.py
+++ items/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'items'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('new',views.new_item,name='new'),
+    path('', views.ItemIndex.as_view(), name='index'),
+    path('<int:pk>/', views.ItemDetail.as_view(), name='detail'),
+    path('new',views.ItemCreate.as_view(),name='new'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
 from django.views import generic
-from django.urls import reverse
+from django.urls import reverse_lazy
 
 from .models import Item
 from .forms import ItemForm
 
-class IndexView(generic.ListView):
+class ItemIndex(generic.ListView):
     template_name = 'items/index.html'
     context_object_name = 'item_list'
 
@@ -15,25 +12,13 @@ class IndexView(generic.ListView):
         return Item.objects.all()
 
 
-class DetailView(generic.DetailView):
+class ItemDetail(generic.DetailView):
     model = Item
     template_name = 'items/detail.html'
 
-def new_item(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
 
-        if form.is_valid():
-            name_item = form.cleaned_data['name_item']
-            quantity_item = form.cleaned_data['quantity_item']
-            item_type = form.cleaned_data['item_type']
-            unity_type = form.cleaned_data['unity_type']
-
-            item = Item.objects.create(name_item=name_item,
-                    quantity_item=quantity_item,item_type=item_type,unity_type=unity_type)
-
-            return HttpResponseRedirect(reverse('items:index'))
-    else:
-        form = ItemForm()
-
-    return render(request,'items/new_item.html', {'form':form})
+class ItemCreate(generic.CreateView):
+    model = Item
+    template_name = 'items/new_item.html'
+    success_url = reverse_lazy('items:index')
+    form_class = ItemForm
