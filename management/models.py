37fc21510af81d513e7579e945a371eabe761d23
matheusbsilva <matheusbattista@hotmail.com>
2018-01-15 11:51:56 -0200

    [UPDATE] Create operation model to management app

--- management/models.py
+++ management/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from items.models import Item
+
+""" Operation types are in portuguese-BR """
+""" 0 for items removed """
+""" 1 for items added """
+OPERATION_TYPES = (
+    ('0','Item retirado'),
+    ('1','Item inserido'),
+)
+
+
+class Operation(models.Model):
+    date = models.DateField(auto_now=True,blank=False,null=False)
+    amount = models.IntegerField(blank=False, null=False)
+    operation_type = models.CharField(
+            max_length=1,
+            choices=OPERATION_TYPES,
+            blank=False,
+            null=False)
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (str(self.date) + '-' + self.item.name_item)
